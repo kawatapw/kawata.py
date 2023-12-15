bd84243defbdb99c1e9aa016684ec46e69b92a83
Austin Steyer <asteyer11@gmail.com>
2023-12-14 23:11:52 -0700

    Fix disclosure.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ of experience interested in hosting their own osu private server instance(s).
 
 the project is developed primarily by the Akatsuki (https://akatsuki.pw) team,
 and our aim is to create the most easily maintainable, reliable, and feature-rich
-osu! server implementation available.
-
+osu! server implementation available. We are fully open source!
+We at Kawata have forked this repository for our uses and 
 we're also fully open source!
 https://github.com/kawatapw/kawata.py
 """
