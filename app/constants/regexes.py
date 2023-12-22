c5ca3d2ac32b7dadf097b1887a41f0466d7d4edb
Austin Steyer <asteyer11@gmail.com>
2023-12-21 19:41:06 -0700

    Fix abypass login

--- app/constants/regexes.py
+++ app/constants/regexes.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import re
 
 OSU_VERSION = re.compile(
-    r"^b(?P<date>\d{8})(?:\.(?P<revision>\d))?"
+    r"^(?:Abypass Client )?b(?P<date>\d{8})(?:\.(?P<revision>\d))?"
     r"(?P<stream>beta|cuttingedge|dev|tourney|Aeris)?$",
 )
 
