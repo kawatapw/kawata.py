40c6f1ac7a7c1557c42098c1788ceb85ac4001bb
Austin Steyer <asteyer11@gmail.com>
2023-12-21 19:41:06 -0700

    Fix abypass login

--- app/constants/regexes.py
+++ app/constants/regexes.py
@@ -6,7 +6,7 @@ __all__ = ("OSU_VERSION", "USERNAME", "EMAIL", "BEST_OF")
 
 
 OSU_VERSION = re.compile(
-    r"^b(?P<date>\d{8})(?:\.(?P<revision>\d))?"
+    r"^(?:Abypass Client )?b(?P<date>\d{8})(?:\.(?P<revision>\d))?"
     r"(?P<stream>beta|cuttingedge|dev|tourney|Aeris)?$",
 )
 
