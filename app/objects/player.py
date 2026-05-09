043e9f8a41df7dd63e03e6dd5a1521492ad2ec9c
The Fantastic Loki <thefantasticloki+github@proton.me>
2026-05-09 04:09:15 -0600

    fix: handle player rejoining match after disconnection
    Added logic to allow players to rejoin a match they were disconnected from by checking if the player is already in the match but disconnected. If so, the player is reconnected to the match chat and the match join success packet is sent. Also added a check in `MatchCreate` to prevent players from joining a new match if they are already in one, and improved error handling in `leave_match` for cases where the player's slot is not found.

--- app/objects/player.py
+++ app/objects/player.py
@@ -826,9 +826,29 @@ class Player:
 
         log(f"Unsilenced {self}.", Ansi.LCYAN)
 
+    def _special_case_disconnected_rejoin(self, match: Match, passwd: str) -> bool:
+        """This method will allow user to rejoin a match they were disconnected from. If everything's working properly, this method should never be called."""
+
+        reverse_slot_search = match.get_slot(self)
+        lobby = app.state.sessions.channels.get_by_name("#lobby")
+        if reverse_slot_search is None:
+            # recoverable, reset user state before perform normal join.
+            self.match = None
+            self.leave_channel(match.chat)
+            self.join_channel(lobby) if lobby else None
+            return self.join_match(match, passwd)
+
+        # user is in the match, but was disconnected.
+        self.join_channel(match.chat)
+        self.leave_channel(lobby) if lobby else None
+        self.enqueue(app.packets.match_join_success(match))
+        return True
+
     def join_match(self, match: Match, passwd: str) -> bool:
         """Attempt to add `self` to `match`."""
         if self.match:
+            if self.match.id == match.id:
+                return self._special_case_disconnected_rejoin(match, passwd)
             log(f"{self} tried to join multiple matches?")
             self.enqueue(app.packets.match_join_fail())
             return False
@@ -889,9 +909,11 @@ class Player:
 
         slot = self.match.get_slot(self)
         if slot is None:
-            raise ValueError("Player slot not found in match")
-
-        if slot.status == SlotStatus.locked:
+            log(
+                f"{self} tried leaving a match, but slot couldn't be found?",
+                Ansi.LYELLOW,
+            )
+        elif slot.status == SlotStatus.locked:
             # player was kicked, keep the slot locked.
             new_status = SlotStatus.locked
         else:
